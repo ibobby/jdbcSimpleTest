8a5b049a0203ca272d8d3f0890b7c4d01c5d5c37
b.istomin <ibobby0386@gmail.com>
2015-06-16 13:41:40 +0300

    form tune

--- src/main/java/MainForm.java
+++ src/main/java/MainForm.java
@@ -7,39 +7,41 @@ import java.awt.event.ActionListener;
  * Created by b.istomin on 09.06.2015.
  */
 public class MainForm extends JFrame {
-    private JButton goButton;
-    private JRadioButton oldJdbc;
-    private JPanel rootPanel;
-    private JProgressBar progressBar1;
-    private JRadioButton springJdbcTemplate;
-    private JList resultList;
-    private JButton getButton;
+        private JButton goButton;
+        private JRadioButton oldJdbc;
+        private JPanel rootPanel;
+        private JProgressBar progressBar1;
+        private JRadioButton springJdbcTemplate;
+        private JList resultList;
+        private JButton getButton;
+    private JScrollBar scrollBar1;
 
     public MainForm() throws HeadlessException {
-        super("some jdbc testing");
-        pack();
-        setContentPane(rootPanel);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 500);
+            super("some jdbc testing");
+            pack();
 
-        goButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (oldJdbc.isSelected()) {
-                    OldJdbc.addStudent();
-                } else if (springJdbcTemplate.isSelected()) {
-                    SpringJdbc.addStudent();
+            setContentPane(rootPanel);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setSize(350, 350);
+
+            goButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (oldJdbc.isSelected()) {
+                        OldJdbc.addStudent();
+                    } else if (springJdbcTemplate.isSelected()) {
+                        SpringJdbc.addStudent();
+                    }
                 }
-            }
-        });
+            });
 
-        getButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                resultList.setListData(SpringJdbc.getStudents().toArray());
-            }
-        });
+            getButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    resultList.setListData(SpringJdbc.getStudents().toArray());
+                }
+            });
 
-        this.setVisible(true);
-    }
+            this.setVisible(true);
+        }
 }
